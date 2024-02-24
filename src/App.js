3fdf1ea32af278f22d602989d4fc19b153526c9e
Praveen Raju <praveenraju2889@gmail.com>
2024-02-25 00:08:36 +0400

    new portfolio created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/NavBar";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { darkTheme, lightTheme } from './utils/Themes.js'
+import { useState, useEffect } from "react";
+import HeroSection from './components/HeroSection/index.js';
+import Skills from './components/Skills/index.js';
+import Experience from  './components/Experience/index.js'
+import Projects from './components/Projects/index.js';
+import Education from "./components/Education";
+import Footer from './components/Footer/index.js';
+import Contact from './components/Contact/index.js';
+import ProjectDetails from "./components/ProjectDetails";
+
+
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router >
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Skills/>
+          <Experience/>
+          
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
+
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/data/constants.js
@@ -0,0 +1,275 @@
+export const Bio = {
+    name: "Praveen Raju",
+    roles: [
+      "Software Engineer",
+      "Front End Developer"
+    ],
+    description:
+      "Kony Mobile/Web Application Developer with over 9 years experience for leading financial and technology enterprises. ",
+    github: "https://github.com/praveenraju2889",
+    resume:
+      "https://drive.google.com/file/d/1iyDn1lwbcvkpPtw7FsAAj7VCTGtGGvIu/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/praveen-raju-67609219/",
+    insta: "https://www.instagram.com/praveenrajuu/"
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "Kony",
+          image:
+            "https://gonzobanker.com/wp-content/uploads//temenos-kony.jpg"
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Xcode",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5UBPtvsJXorcTBEaZoLwFXUQ3XfY1i0nkZJ_iuszhig&s"
+        },
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "Swift",
+          image:
+            "https://developer.apple.com/swift/images/swift-og.png",
+        }
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Java Spring Boot",
+          image: "https://pbs.twimg.com/profile_images/1235868806079057921/fTL08u_H_400x400.png",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "PL/SQL Developer",
+          image:
+            "https://www.appdeploynews.com/wp-content/uploads/2023/03/plsql-icon.png",
+        },
+        {
+          name: "APACHE KAFKA",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3J8_6ncmIy43muYEyLBvzOt0BaU2wR3Dma1670eF1yA&s",
+        },
+        {
+          name: "RDS",
+          image:
+            "https://logowik.com/content/uploads/images/aws-rds2214.logowik.com.webp",
+        },
+        {
+          name: "JUnit",
+          image:
+            "https://miro.medium.com/v2/resize:fit:1144/1*Xhct1rcbMzmbedNWfwLxIw.png",
+        }
+      ],
+    },
+    {
+      title: "DevOps",
+      skills: [
+        {
+          name: "AWS",
+          image:
+            "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+        },
+        {
+          name: "Asure",
+          image:
+            "https://pbs.twimg.com/profile_images/1145617831905681408/XNKktHjN_400x400.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+          name: "Openshift",
+          image:
+            "https://miro.medium.com/v2/resize:fit:719/1*Ug9xmJQWZM0lWnLvKSd42Q.png",
+        }
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "TFS",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfcpdq0lDDk-cnJT20eSCf_PFOu_CGgvH2eKf1a-uZbzDkC_Kq_ZNfMKdLiLc7PAvYsks&usqp=CAU",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "SVN",
+          image:
+            "https://live.staticflickr.com/8376/8469610561_79347bc648_b.jpg",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "Jira",
+          image:
+            "https://www.automation-consultants.com/wp-content/uploads/2019/06/jira-bpm-2.png",
+        }
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://www.adcb.com/en/Images/ADCB_Master_CMYK_Pos-thumbnail_tcm41-412485.png",
+      role: "Senior System Analyst",
+      company: "Abu Dhabi Commercial Bank - Abu Dhabi, UAE",
+      date: "Dec 2018 - Present",
+      desc: "Worked for the client Abu Dhabi Commercial Bank in Customer onboarding application which is a responsive web application supports in Desktop web, Tablet web and Mobile web.",
+      skills: [
+        "Kony",
+        "JavaScript",
+        "Java Spring Boot",
+        "PL/SQL Developer",
+        "HTML",
+        "CSS",
+        "IOS",
+        "Asure Dev Ops"     
+      ],
+     // doc: "",
+    },
+    {
+      id: 1,
+      img: "https://pbs.twimg.com/profile_images/1189123545793609729/JmLtnrXS_400x400.png",
+      role: "Senior Software Engineer",
+      company: "Enterprise Touch / TrueTech Solutions - Chennai, India",
+      date: "Feb 2014 - Oct 2018",
+      desc: "Extensively involved in Design, development and deployed mobile applications for various clients such as Ultra Tech Cements (India), HMC (Qatar) On-Site, Mofa Qatar, Qrail, College of North Atlantic - Qatar (Offsite).",
+      skills: [
+        "Kony",
+        "JavaScript",
+        "IOS",
+        "Xcode"
+      ],
+      //doc: "",
+    }
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/C510BAQGzyYPO_FA4kQ/company-logo_200_200/0/1630631497296?e=2147483647&v=beta&t=ThCdJKhkxXTF3QNBhrk4-TKjR_ZwgyRDzhWpNT0AuTo",
+      school: "Dr. G.R Damodaran College of Science, Bharathiyar University",
+      date: "May 2010 - April 2013",
+      grade: "76%",
+      degree: "Master of Computer Application",
+    },
+    {
+      id: 1,
+      img: "https://www.kongunaducollege.ac.in/sites/kongunaducollege.ac.in/files/kongunadu_logo_final.png",
+      school: "Kongunadu Arts and Science, Bharathiyar University",
+      date: "Apr 2007 - Apr 2010",
+      grade: "70%",
+      //desc: "",
+      degree: "Bachelor of Science, Computer Technology",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_hdVm2EAfR6drOrXzlkhb7CHeHyHXSbp2FJGY7DuxEm8y6fwfm4_nhCXjuCG5YDHk4Kg&usqp=CAU",
+      school: "Green Valley Matriculation and Higher Secondary School",
+      date: "Apr 2006 - Apr 2007",
+      grade: "77%",
+      //desc: "",
+      degree: "High School, Computer Science",
+    }
+  ];
+  
+  export const projects = [
+    {
+      id: 11,
+      title: "Customer onboarding platform",
+      date: "June 2022 - Present",
+      description:
+        "Customer onboarding, applying for credit card, personal.",
+      image:
+        "https://digitalapps.adcb.com/apps/ADCBCOP/1809171584/desktopweb/images/customerlandingbg.jpg",
+      tags: [
+        "Kony",
+        "JavaScript",
+        "Java Spring Boot",
+        "PL/SQL Developer",
+      ],
+      category: "web app",
+      //github: "",
+      webapp: "https://digitalapps.adcb.com/apps/ADCBCOP/",
+    },
+    {
+      id: 9,
+      title: "Mobile Internet Banking",
+      date: "June 2021 - June 2022",
+      description:
+        "ADCB Mobile Internet banking for transactions, bill payment, add beneficiary, CC bill payment etc.",
+      image:
+        "https://play-lh.googleusercontent.com/HxBhzp5ik9gRaBihF4713frSxXXus_vcc7OUArNEFxgOD9DBYd2AWgUc5l23uPHotrI=w5120-h2880-rw",
+      tags: [
+        "Kony",
+        "JavaScript",
+        "Android",
+        "IOS"
+      ],
+      category: "native app",
+      //github: "",
+      webapp: "https://play.google.com/store/apps/details?id=com.adcb.bank",
+    },
+    {
+      id: 9,
+      title: "Hayyak",
+      date: "June 2020 - June 2021",
+      description:
+        "ADCB Mobile Internet banking for transactions, bill payment, add beneficiary, CC bill payment etc.",
+      image:
+        "https://play-lh.googleusercontent.com/DZx688lsfzwZIllzX0mK2-Uh4QwZrG8-iuoj7CzRmN8xQDi-5Rmq4W07dqQ4UGrADAo=w5120-h2880-rw",
+      tags: [
+        "Kony",
+        "JavaScript",
+        "Android",
+        "IOS"
+      ],
+      category: "native app",
+      //github: "",
+      webapp: "https://play.google.com/store/apps/details?id=com.adcb.cbgdigi&hl=en&gl=US",
+    }
+  ];
+  
+ 
